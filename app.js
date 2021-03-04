d93f391cfb0d3fd2ca900af5db6fe5e63eea1db8
xueqian chen <xueqianchen@xueqians-MacBook-Air.local>
2021-03-03 23:46:02 -0700

    added new css and js folders

---
+++ app.js
@@ -0,0 +1,10 @@
+const topLink = document.querySelector(".top-link");
+window.addEventListener('scroll',function(){
+    const scrollHeight = window.pageYOffset;
+    if(scrollHeight > 500){
+        topLink.classList.add("show-link");
+    }
+    else {
+        topLink.classList.remove("show-link");
+    }
+});
